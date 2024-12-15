d0bab021683b55460352351e98eaad3fbc97ccab
emilciaaa <emilia_kreft@wp.pl>
2024-12-15 18:03:05 +0100

    updated function get_model_DecisionTree

--- src/DecisionTree.py
+++ src/DecisionTree.py
@@ -6,7 +6,7 @@ def get_model_DecisionTree(path_to_dataset):
     data = pd.read_csv(path_to_dataset)
     x = data.drop(columns=['animal_name', 'class_type'])
     y = data['class_type']
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+    x_train, x_test, y_train, y_test = split_data()
     model = DecisionTreeClassifier()
     model.fit(x_train, y_train)
 
