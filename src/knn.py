cdb318c2d0acc4da416afae58d46e7e6d4447e62
=cynka02 <=cynka02@interia.pl>
2024-12-15 16:31:39 +0100

    Improved version of knn.py

--- src/knn.py
+++ src/knn.py
@@ -1,17 +1,7 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-
-
-def accuracy_of_model(model, test, test_classes):
-    prediction = model.predict(test)
-    test_classes = test_classes.tolist()
-
-    n = len(prediction)
-    correct = 0
-    for i in range(n):
-        correct += test_classes[i] == prediction[i]
-    return correct / n
+from sklearn.metrics import accuracy_score
 
 
 def find_best_k(train, test, train_classes, test_classes):
@@ -20,7 +10,8 @@ def find_best_k(train, test, train_classes, test_classes):
     for k in range(1, len(train)):
         knn = KNeighborsClassifier(n_neighbors=k)
         knn.fit(train, train_classes)
-        accuracy_list.append(accuracy_of_model(knn, test, test_classes))
+        prediction = knn.predict(test)
+        accuracy_list.append(accuracy_score(test_classes, prediction))
 
     return accuracy_list.index(max(accuracy_list)) + 1
 
@@ -29,7 +20,7 @@ def get_model_knn():
     data = pd.read_csv('../data/zoo.csv')
     data = data.drop('animal_name', axis=1)
 
-    train, test = train_test_split(data, test_size=0.2, random_state=42)
+    train, test = train_test_split(data, test_size=0.3, random_state=42)
     train_classes = train['class_type']
     train = train.drop('class_type', axis=1)
     test_classes = test['class_type']
