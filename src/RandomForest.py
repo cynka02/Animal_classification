ff97bdd7aefedf54ffe039e6e8a997a165488740
emilciaaa <emilia_kreft@wp.pl>
2024-12-15 18:14:07 +0100

    updated packages in RandomForest.py

--- src/RandomForest.py
+++ src/RandomForest.py
@@ -1,6 +1,5 @@
-import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from load_dataset import split_data
 
 
 def get_model_RandomForest(path_to_dataset):
