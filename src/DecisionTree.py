5cfeccc3a978d187bab19b46f7a44a1e597a332f
emilciaaa <emilia_kreft@wp.pl>
2024-12-15 16:14:46 +0100

    added file DecisionTree.py with get_model_DecisionTree function

--- src/DecisionTree.py
+++ src/DecisionTree.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
-
 def get_model_DecisionTree(dataset, columns_to_drop, target):
     data = pd.read_csv(dataset)
     X = data.drop(columns=columns_to_drop)
