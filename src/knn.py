4ae52342da5f81722583480bb96a234009be97cd
=cynka02 <=cynka02@interia.pl>
2024-12-15 15:28:07 +0100

    Created structure of kNN model

---
+++ src/knn.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from train_test_split import get_data
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def find_best_k(train, test):
+    # create models for each k
+    # return k
+    pass
+
+
+def get_model_knn():
+    # import data
+    # find best k
+    # create model with best k
+    # return model
+    pass
+
+
+def main():
+
+    get_model_knn()
+
+
+if __name__ == "__main__":
+    main()
