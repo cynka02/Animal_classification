3c58a6da2154dbca51150c97971c5a7873f4ad1d
=cynka02 <=cynka02@interia.pl>
2024-12-15 16:51:29 +0100

    Added file evaulate_models.py

---
+++ src/evaluate_models.py
@@ -0,0 +1,33 @@
+from knn import get_model_knn
+from DecisionTree import get_model_DecisionTree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+def compute_accuracy(model, test):
+    test_classes = test['class_type']
+    test = test.drop(columns=['class_type', 'animal_name'])
+
+    return accuracy_score(test_classes, model.predict(test))
+
+
+def evaluate_models(path_to_dataset):
+    data = pd.read_csv(path_to_dataset)
+    train, test = train_test_split(data, test_size=0.3, random_state=42)
+
+    knn = get_model_knn(path_to_dataset)
+    decision_tree = get_model_DecisionTree(path_to_dataset)
+
+    models_evaulation = {'kNN': compute_accuracy(knn, test),
+                         'Decision Tree': compute_accuracy(decision_tree, test)}
+
+    print(models_evaulation)
+
+def main():
+
+    evaluate_models('../data/zoo.csv')
+
+
+if __name__ == "__main__":
+    main()
