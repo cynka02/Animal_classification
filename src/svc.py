8b33f12d4e504154510ba785774b20f1e89e1619
emilciaaa <emilia_kreft@wp.pl>
2024-12-15 18:11:15 +0100

    updated file svc.py

--- src/svc.py
+++ src/svc.py
@@ -1,19 +1,15 @@
 from sklearn.svm import SVC
 from load_dataset import split_data
 
-def get_model_SVC(path_to_dataset):
-    data = pd.read_csv(path_to_dataset)
-    x = data.drop(columns=['animal_name', 'class_type'])
-    y = data['class_type']
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+def get_model_SVC():
+    x_train, x_test, y_train, y_test = split_data()
     model = SVC(probability=True)
     model.fit(x_train, y_train)
 
     return model
 
 def main():
-    path_to_dataset = '../data/zoo.csv'
-    get_model_SVC(path_to_dataset)
+    get_model_SVC()
 
 if __name__ == "__main__":
     main()
