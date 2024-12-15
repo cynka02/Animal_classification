bc6abef5a8b9fbd9297d842ec9586f86cb71d507
=cynka02 <=cynka02@interia.pl>
2024-12-15 15:52:23 +0100

    Updated version of knn.py

--- src/knn.py
+++ src/knn.py
@@ -3,23 +3,32 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
 
-def find_best_k(train, test, classes):
+def accuracy_of_model(model, test):
+    # test model
+    # return percent of accuracy
+    pass
 
-    # create models for each k
+def find_best_k(train, test, classes):
+    for k in range(1, 10):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(train, classes)
+        #
     # return k
     pass
 
 
 def get_model_knn():
     data = pd.read_csv('../data/zoo.csv')
+    classes = pd.read_csv('../data/class.csv')
     data = data.drop('animal_name', axis=1)
+    classes = classes['Class_Number']
 
     train, test = train_test_split(data, test_size=0.2)
 
     k = find_best_k(train, test)
 
     knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(train, data['class_type'])
+    knn.fit(train, classes)
 
     return knn
 
