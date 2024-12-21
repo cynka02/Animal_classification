a883fd95e5b5b16a64c8ec6bad23dbdcdde2fb90
=cynka02 <=cynka02@interia.pl>
2024-12-21 22:31:40 +0100

    Working version of animal guessing

--- src/guess_animal.py
+++ src/guess_animal.py
@@ -14,9 +14,10 @@ def ask_question(data):
         if column not in ('animal_name', 'legs'):
             variety[column] = sum(data[column]) / len(data)
     feature = find_nearest_to_50(variety)
-    answer = input('Is your animal a ' + feature + '?')
-    answer = 1 if 'Y' else 0
+    answer = input('Is your animal a ' + feature + ' (Yes/No)?\n')
+    answer = 1 if answer == 'Yes' else 0
     data = data[data[feature] == answer].drop(columns=feature)
+    print(data)
     return data
 
 
@@ -24,12 +25,14 @@ def guess_animal():
     data = load_data()
     while len(data) > 1:
         data = ask_question(data)
-    return data['animal_name'][0]
+    if len(data) == 0:
+        return "I don't know about such animal"
+    return data['animal_name']
 
 
 def main():
 
-    ask_question(load_data())
+    print(guess_animal())
 
 
 if __name__ == '__main__':
