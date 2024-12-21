750b7ae3db7a1a12f4859dd7576df93ff5e93875
=cynka02 <=cynka02@interia.pl>
2024-12-21 21:25:53 +0100

    Added function load_data

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -8,3 +8,9 @@ def split_data():
     x = data.drop(columns=COLUMN_TO_DELETE)
     y = data[CLASS_COLUMN]
     return train_test_split(x, y, test_size=TEST_SIZE, random_state=SEED)
+
+
+def load_data():
+    data = pd.read_csv(PATH_TO_DATASET)
+    data = data.drop(columns='class_type')
+    return data
