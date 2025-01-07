d0e1e36ce4993a619776b8e9d2b1aeb504e13fdb
emilciaaa <emilia_kreft@wp.pl>
2025-01-07 09:32:13 +0100

    updated file DecisionTree.py

--- src/models/DecisionTree.py
+++ src/models/DecisionTree.py
@@ -2,7 +2,13 @@ from sklearn.tree import DecisionTreeClassifier
 from src.load_dataset import split_data
 
 
-def get_model_DecisionTree():
+def get_model_decision_tree():
+    """
+        Trains a Decision Tree Classifier model.
+
+        Returns:
+            DecisionTreeClassifier: A trained Decision Tree Classifier model.
+    """
     x_train, x_test, y_train, y_test = split_data()
     model = DecisionTreeClassifier()
     model.fit(x_train, y_train)
@@ -10,7 +16,7 @@ def get_model_DecisionTree():
 
 
 def main():
-    get_model_DecisionTree()
+    get_model_decision_tree()
 
 
 if __name__ == "__main__":
