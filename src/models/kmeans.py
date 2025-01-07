3cbae3ed01b1bced8bc4b67717696265b9e042e8
emilciaaa <emilia_kreft@wp.pl>
2025-01-07 09:31:22 +0100

    updated kmeans.py

---
+++ src/models/kmeans.py
@@ -0,0 +1,23 @@
+from sklearn.cluster import KMeans
+from src.load_dataset import split_data
+
+
+def get_model_kmeans():
+    """
+        Trains a KMeans clustering model.
+
+        Returns:
+            KMeans: A trained KMeans clustering model.
+    """
+    x_train, x_test, y_train, y_test = split_data()
+    model = KMeans(n_clusters=7)
+    model.fit(x_train)
+    return model
+
+
+def main():
+    get_model_kmeans()
+
+
+if __name__ == "__main__":
+    main()
