a631c885c63d4f1e542b3e0e67d7c173cd84d11a
emilciaaa <emilia_kreft@wp.pl>
2025-01-07 09:32:31 +0100

    updated file evaluate_models.py

--- src/evaluate_models.py
+++ src/evaluate_models.py
@@ -1,7 +1,9 @@
 from models.knn import get_model_knn
-from models.DecisionTree import get_model_DecisionTree
-from models.svc import get_model_SVC
-from models.RandomForest import get_model_RandomForest
+from models.DecisionTree import get_model_decision_tree
+from models.svc import get_model_svc
+from models.RandomForest import get_model_random_forest
+from models.kmeans import get_model_kmeans
+from models.GradientBoosting import get_model_gradient_boosting
 from load_dataset import split_data
 from sklearn.metrics import accuracy_score
 
@@ -14,16 +16,20 @@ def evaluate_models():
     x_train, x_test, y_train, y_test = split_data()
 
     knn = get_model_knn()
-    decision_tree = get_model_DecisionTree()
-    svc = get_model_SVC()
-    random_forest = get_model_RandomForest()
+    decision_tree = get_model_decision_tree()
+    svc = get_model_svc()
+    random_forest = get_model_random_forest()
+    kmeans = get_model_kmeans()
+    gradient_boosting = get_model_gradient_boosting()
 
-    models_evaulation = {'kNN': accuracy_score(y_test, knn.predict(x_test)),
+    models_evaluation = {'kNN': accuracy_score(y_test, knn.predict(x_test)),
                          'Decision Tree': accuracy_score(y_test, decision_tree.predict(x_test)),
                          'SVC': accuracy_score(y_test, svc.predict(x_test)),
-                         'Random Forest': accuracy_score(y_test, random_forest.predict(x_test))}
+                         'Random Forest': accuracy_score(y_test, random_forest.predict(x_test)),
+                         'KMeans': accuracy_score(y_test, kmeans.predict(x_test)),
+                         'Gradient Boosting': accuracy_score(y_test, gradient_boosting.predict(x_test))}
 
-    print(models_evaulation)
+    print(models_evaluation)
 
 
 def main():
