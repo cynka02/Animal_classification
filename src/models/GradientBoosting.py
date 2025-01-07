245f739130106b15d44b6f24484778f41f275fb9
emilciaaa <emilia_kreft@wp.pl>
2025-01-07 09:31:52 +0100

    updated file GradientBoosting.py

---
+++ src/models/GradientBoosting.py
@@ -0,0 +1,23 @@
+from sklearn.ensemble import GradientBoostingClassifier
+from src.load_dataset import split_data
+
+
+def get_model_gradient_boosting():
+    """
+        Trains a Gradient Boosting Classifier model.
+
+        Returns:
+            GradientBoostingClassifier: A trained Gradient Boosting Classifier model.
+    """
+    x_train, x_test, y_train, y_test = split_data()
+    model = GradientBoostingClassifier()
+    model.fit(x_train, y_train)
+    return model
+
+
+def main():
+    get_model_gradient_boosting()
+
+
+if __name__ == "__main__":
+    main()
