ae9b5e3d7bad8bbec199e671aadc44c89db9df2a
=cynka02 <=cynka02@interia.pl>
2025-01-10 16:20:30 +0100

    Added duplicated rows drop & docstring

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -4,7 +4,14 @@ from sklearn.model_selection import train_test_split
 
 
 def split_data():
+    """
+        Splits data into train and test dataframes
+
+        Returns:
+            tuple with train and test dataframes
+    """
     data = pd.read_csv(PATH_TO_DATASET)
+    data = data.drop_duplicates()
     x = data.drop(columns=COLUMN_TO_DELETE)
     y = data[CLASS_COLUMN]
     return train_test_split(x, y, test_size=TEST_SIZE, random_state=SEED)
@@ -12,4 +19,5 @@ def split_data():
 
 def load_data():
     data = pd.read_csv(PATH_TO_DATASET)
+    data = data.drop_duplicates()
     return data
