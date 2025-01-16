c093aa73b5ed0921c356e9b4bf5a38d5432f7304
=cynka02 <=cynka02@interia.pl>
2025-01-16 13:10:22 +0100

    Updated filenames

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 COLUMN_TO_DELETE = ['animal_name', 'class_type']
 CLASS_COLUMN = 'class_type'
 PATH_TO_DATASET = 'data/zoo.csv'
+PATH_TO_CLASS_DATASET = 'data/class.csv'
 OUTPUT_FILE_PATH = 'out/accuracy.json'
 SEED = 42
 TEST_SIZE = 0.3
