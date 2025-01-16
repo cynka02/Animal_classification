0e207648ddb51b9c166d1d05e7aff00f6547bafd
emilciaaa <emilia_kreft@wp.pl>
2025-01-16 22:56:43 +0100

    updated file load_dataset.py

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -4,6 +4,19 @@ from sklearn.model_selection import train_test_split
 from utils import get_repo_path
 
 
+def load_data():
+    """
+        Loads the dataset.
+
+        Returns:
+            DataFrame: A DataFrame containing the dataset.
+    """
+
+    data = pd.read_csv(get_repo_path() / PATH_TO_DATASET)
+    data = data.drop_duplicates()
+    return data
+
+
 def split_data():
     """
         Splits data into train and test dataframes.
@@ -17,9 +30,3 @@ def split_data():
     x = data.drop(columns=COLUMN_TO_DELETE)
     y = data[CLASS_COLUMN]
     return train_test_split(x, y, test_size=TEST_SIZE, random_state=SEED)
-
-
-def load_data():
-    data = pd.read_csv(get_repo_path() / PATH_TO_DATASET)
-    data = data.drop_duplicates()
-    return data
