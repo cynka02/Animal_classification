f9c54a62e230347566b38cc9d1ebaf5d77f122ab
emilciaaa <emilia_kreft@wp.pl>
2025-01-28 22:16:10 +0100

    added docstring file get_model_knn_test.py

--- src/tests/get_model_knn_test.py
+++ src/tests/get_model_knn_test.py
@@ -7,6 +7,10 @@ from models.kNN import get_model_knn
 
 class TestKNNModel(unittest.TestCase):
     def test_return_type(self):
+        """
+            Test if get_model_knn returns an instance of KNeighborsClassifier.
+        """
+
         model = get_model_knn()
         self.assertIsInstance(model, KNeighborsClassifier)
 
