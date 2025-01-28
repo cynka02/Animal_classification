9053a116cb71947e03f81cbd5ed10c80a05ec33d
emilciaaa <emilia_kreft@wp.pl>
2025-01-28 22:01:04 +0100

    added docstring to file get_model_decision_tree_test.py

--- src/tests/get_model_decision_tree_test.py
+++ src/tests/get_model_decision_tree_test.py
@@ -7,6 +7,9 @@ from models.DecisionTree import get_model_decision_tree
 
 class TestDecisionTreeModel(unittest.TestCase):
     def test_return_type(self):
+        """
+            Test if get_model_decision_tree returns an instance of DecisionTreeClassifier.
+        """
         model = get_model_decision_tree()
         self.assertIsInstance(model, DecisionTreeClassifier)
 
